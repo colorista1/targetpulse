6506ec73af1ee0efd9520c65a2c330a5afd81980
Deazlime <200516049bulatov@gmail.com>
2025-05-15 19:27:59 +0500

    Добавлены модели для досок и задач, реализованы представления и маршруты для управления досками и задачами, обновлены шаблоны. Обновлен файл базы данных.

--- targetpulse/urls.py
+++ targetpulse/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('admin-page/', views.admin_page, name='admin_page'),
     path('profile/', views.profile_view, name='profile'),
+    path('boards/create/', views.board_create, name='board_create'),
+    path('boards/<int:pk>/edit/', views.board_edit, name='board_edit'),
+    path('boards/<int:pk>/delete/', views.board_delete, name='board_delete'),
+    path('boards/<int:pk>/add_member/', views.add_member, name='add_member'),
+    path('boards/<int:pk>/', views.board_detail, name='board_detail'),
+    path('boards/<int:board_pk>/tasks/create/', views.task_create, name='task_create'),
+    path('boards/<int:board_pk>/tasks/<int:task_pk>/edit/', views.task_edit, name='task_edit'),
+    path('boards/<int:board_pk>/tasks/<int:task_pk>/delete/', views.task_delete, name='task_delete'),
+    path('boards/<int:board_pk>/tasks/<int:task_pk>/change_status/', views.change_task_status, name='change_task_status'),
 ]
